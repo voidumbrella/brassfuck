371eaba9b7d1efffb6ed44112aecebc2bb035d8d
voidumbrella <voidumbrella@gmail.com>
2020-01-05 16:51:03 -0500

    Perform basic optimizations

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,20 @@ struct Interpreter {
 
 #[derive(Debug)]
 enum Op {
-    MoveRight,
-    MoveLeft,
-    IncValue,
-    DecValue,
+    MoveRight(usize),
+    MoveLeft(usize),
+    Add(u8),
+    Sub(u8),
     Output,
     Input,
     OpenLoop(usize),
     CloseLoop(usize),
+
+    // Optimizations of common idioms
+    Clear,
 }
 
-const DEFAULT_MEMORY_SIZE: usize = 30_000;
+const DEFAULT_MEMORY_SIZE: usize = 65_536;
 
 impl Interpreter {
     fn new() -> Interpreter {
@@ -37,21 +40,15 @@ impl Interpreter {
             let cell = &mut self.memory[self.mp];
 
             match *instruction {
-                Op::MoveRight => {
-                    if self.mp == self.memory.len() { self.mp = 0; }
-                    else { self.mp += 1; }
-                    // self.mp = (self.mp + 1) % self.memory.len();
-                }
-                Op::MoveLeft => {
-                    if self.mp == 0 { self.mp = self.memory.len() - 1; }
-                    else { self.mp -= 1; }
-                    // self.mp = (self.mp - 1) % self.memory.len();
-                }
+                Op::MoveRight(n) =>
+                    self.mp += n,
+                Op::MoveLeft(n) =>
+                    self.mp -= n,
 
-                Op::IncValue =>
-                    *cell = cell.wrapping_add(1),
-                Op::DecValue =>
-                    *cell = cell.wrapping_sub(1),
+                Op::Add(n) =>
+                    *cell = cell.wrapping_add(n),
+                Op::Sub(n) =>
+                    *cell = cell.wrapping_sub(n),
 
                 Op::Output => {
                     print!("{}", *cell as char);
@@ -72,18 +69,30 @@ impl Interpreter {
                 Op::CloseLoop(goto) => {
                     if *cell != 0 { self.ip = goto; }
                 },
+
+                Op::Clear => *cell = 0,
             }
         }
     }
+}
+
+fn match_ops(source: &[char], to_match: &str) -> bool {
+    let mut iter = to_match.chars().zip(source.iter());
+    let mut i = 0;
+    while let Some((s, c)) = iter.next() {
+        if s != *c { return false; }
+        i += 1;
+    }
+    i == to_match.len()
 }
 
 fn parse(source: &str) -> Vec<Op> {
     let mut instructions = Vec::new();
 
     // Stack to store loops
-    let mut stack = Vec::new();
+    // let mut stack = Vec::new();
 
-    let ops: Vec<char> = source
+    let source: Vec<char> = source
         .chars()
         .filter(|c| match *c {
             '>' | '<' | '+' | '-' | '.' | ',' | '[' | ']' => true,
@@ -91,41 +100,78 @@ fn parse(source: &str) -> Vec<Op> {
         })
         .collect();
 
-    let mut iter = ops.iter().enumerate();
-    while let Some((i, op)) = iter.next() {
-        match op {
-            '>' => instructions.push(Op::MoveRight),
-            '<' => instructions.push(Op::MoveLeft),
-            '+' => instructions.push(Op::IncValue),
-            '-' => instructions.push(Op::DecValue),
+    // Compress instructions
+    let mut iter = source.iter().enumerate().peekable();
+    while let Some((i, c)) = iter.next() {
+        let mut n = 1;
+        match c {
+            '>' => {
+                while let Some((_, '>')) = iter.peek() {
+                    iter.next();
+                    n += 1;
+                }
+                instructions.push(Op::MoveRight(n));
+            }
+            '<' => {
+                while let Some((_, '<')) = iter.peek() {
+                    iter.next();
+                    n += 1;
+                }
+                instructions.push(Op::MoveLeft(n));
+            }
+            '+' => {
+                while let Some((_, '+')) = iter.peek() {
+                    iter.next();
+                    n += 1;
+                }
+                instructions.push(Op::Add(n as u8));
+            }
+            '-' => {
+                while let Some((_, '-')) = iter.peek() {
+                    iter.next();
+                    n += 1;
+                }
+                instructions.push(Op::Sub(n as u8));
+            }
             '.' => instructions.push(Op::Output),
             ',' => instructions.push(Op::Input),
+
+            // Loop instructions are stored with temporary values since
+            // the address to jump to are unknown currently
+            // Kinda weird but can't think of a better design right now
             '[' => {
-                // Search for address of end of loop
-                let mut depth = 0;
-                for j in i+1..ops.len() {
-                    if ops[j] == ']' {
-                        if depth == 0 {
-                            // Push address of start of loop onto stack
-                            stack.push(i);
-                            instructions.push(Op::OpenLoop(j));
-                            break;
-                        }
-                        else { depth -= 1; }
-                    } else if ops[j] == '[' { depth += 1; }
+                if match_ops(&source[i+1..], "-]") {
+                    instructions.push(Op::Clear);
+                    iter.nth(1);
                 }
-            },
-            ']' => {
-                // Pop the address of start of loop block from stack.
-                let open_index = match stack.pop() {
-                    None => panic!("unexpected end of loop"),
-                    Some(x) => x,
-                };
-                instructions.push(Op::CloseLoop(open_index));
-            },
+                else { instructions.push(Op::OpenLoop(0)); }
+            }
+            ']' => instructions.push(Op::CloseLoop(0)),
             _ => (),
         }
     }
+
+    // TODO: Syntax errors should not panic
+    
+    // Determine the addresses where each loop instructions should jump to.
+    let mut stack: Vec<(usize, &mut Op)> = Vec::new();
+    let mut iter = instructions.iter_mut().enumerate();
+    while let Some((i, op)) = iter.next() {
+        // Push address of any start of loops onto the stack.
+        // If an end of loop is found, pop the matching start
+        // and set the jump addresses for both to each other.
+        match op {
+            Op::OpenLoop(_) => stack.push((i, op)),
+            Op::CloseLoop(_) => {
+                let (goto, open) = stack.pop()
+                    .unwrap_or_else(|| panic!("Unexpected end of loop"));
+                *op = Op::CloseLoop(goto);
+                *open = Op::OpenLoop(i);
+            }
+            _ => (),
+        }
+    }
+    assert!(stack.is_empty(), "Unterminated loop");
 
     instructions
 }
